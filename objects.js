43bb3c3b72c18a64a829d95803a38e84a01ce72e
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-22 02:43:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(list,artistName,songTitle){
      return Object.assign(list, { [artistName]: songTitle })
 }
 
-function removeFromPlaylist(Playlist,artistName){
+function removeFromPlaylist(playlist,artistName){
    delete playlist.artistName
   return playlist.artistName
 }
